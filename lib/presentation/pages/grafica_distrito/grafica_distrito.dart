2d6e760d4b89068451ce48711597a53f40fa6ae7
pmezquita <ing.pedromezquita@gmail.com>
2024-05-13 12:43:45 -0600

    [UPD] Se agrega total en entrevistados

--- lib/presentation/pages/grafica_distrito/grafica_distrito.dart
+++ lib/presentation/pages/grafica_distrito/grafica_distrito.dart
@@ -2,9 +2,11 @@ import 'package:fenix/presentation/pages/grafica_distrito/grafica_distrito_provi
 import 'package:fenix/presentation/pages/grafica_distrito/widgets/grafico.dart';
 import 'package:fenix/presentation/shared/msg.dart';
 import 'package:fenix/presentation/shared/drawer/drawer.dart';
+import 'package:fenix/presentation/shared/sized_box_height.dart';
 import 'package:fenix/presentation/shared/sliver_app_bar/sliver_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 
 import '../../shared/progress_indicator.dart';
 
@@ -15,6 +17,7 @@ class GraficaDistrito extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final resultados = ref.watch(resultadosDistritoProvider);
     final event = ref.read(resultadosDistritoProvider.notifier);
+    final formatter = NumberFormat.currency(locale: 'en_US', symbol: '', decimalDigits: 0);
 
     return Scaffold(
       drawer: const CustomDrawer(),
@@ -36,8 +39,20 @@ class GraficaDistrito extends ConsumerWidget {
                   if (data == null) return [const Msg(msg: 'Error descargando resultados')];
                   if (data.isEmpty) return [const Msg(msg: 'Sin resultados')];
 
+                  int ttl = 0;
+                  for (final item in data) {
+                    ttl += item.existente ?? 0;
+                  }
+
                   return [
                     Grafico(data),
+                    const SizedBoxHeight(),
+                    Center(
+                      child: Text(
+                        'Total: ${formatter.format(ttl)}',
+                        style: Theme.of(context).textTheme.displayLarge,
+                      ),
+                    ),
                   ];
                 },
                 error: (error, stackTrace) => [Msg(msg: error.toString())],
